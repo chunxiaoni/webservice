917029c224d1465c164ea683f367cd510982b7e0
迭代 <35528065+chunxiaoni@users.noreply.github.com>
2019-07-12 19:12:02 +0800

    Add files via upload

---
+++ repertory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pharmaceuticalfactory
+{
+    public class repertory
+    {
+        public int devicenumber { get; set; }
+        public string drugclasses { get; set; }
+        public int number { get; set; }
+        public string spare1 { get; set; }
+        public string spare2 { get; set; }
+    }
+}
